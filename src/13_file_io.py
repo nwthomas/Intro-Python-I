9c71e09a03760fa65cb4f32cd508f066baf1276e
Nathan Thomas <nwthomas@me.com>
2019-09-01 14:28:19 -0700

    Add in all necessary file operations for file_io.py

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -9,10 +9,22 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # Print all the contents of the file, then close the file
 
 # YOUR CODE HERE
+with open("foo.txt") as f:
+    for line in f:
+        print(line, end="")
 
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
-# then close the file. Open up "bar.txt" and inspect it to make 
+# then close the file. Open up "bar.txt" and inspect it to make
 # sure that it contains what you expect it to contain
 
 # YOUR CODE HERE
+with open("bar.txt", "w") as b:
+    b.write("The ships hung in the sky in much the same way that bricks don't.\n")
+    b.write("Don't Panic.\n")
+    b.write("If there's anything more important than my ego around, I want it caught and shot now.\n")
+    b.close()
+
+with open("bar.txt") as t:
+    for line in t:
+        print(line, end="")
